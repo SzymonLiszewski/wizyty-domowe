aec2d3263728726f16a1737e14bd425fac5ecb89
Szymon <szymonlis21@gmail.com>
2024-12-13 10:21:10 +0100

    initialize spring project

---
+++ backend/home-visits/src/main/java/com/medical/homevisits/home_visits/HomeVisitsApplication.java
@@ -0,0 +1,13 @@
+package com.medical.homevisits.home_visits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeVisitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeVisitsApplication.class, args);
+	}
+
+}
