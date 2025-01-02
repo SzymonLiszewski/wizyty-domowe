8beb8278a61b90e0e760fcb91a8b16a5a38cdd03
pawelpp7 <147920372+pawelpp7@users.noreply.github.com>
2025-01-02 22:25:02 +0100

    Update AuthController.java

--- backend/auth/src/main/java/com/medical/homevisits/auth/user/controller/AuthController.java
+++ backend/auth/src/main/java/com/medical/homevisits/auth/user/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-
+import jakarta.validation.constraints.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
