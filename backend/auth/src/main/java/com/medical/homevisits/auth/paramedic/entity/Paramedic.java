4afe4b1393a37809549c7c26a29659fa5e3c8859
pawelpp7 <147920372+pawelpp7@users.noreply.github.com>
2025-01-05 15:54:33 +0100

    Update Paramedic.java

--- backend/auth/src/main/java/com/medical/homevisits/auth/paramedic/entity/Paramedic.java
+++ backend/auth/src/main/java/com/medical/homevisits/auth/paramedic/entity/Paramedic.java
@@ -1,17 +1,20 @@
 package com.medical.homevisits.auth.paramedic.entity;
 
+import com.medical.homevisits.auth.patient.entity.Patient;
 import com.medical.homevisits.auth.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
 @DiscriminatorValue("Paramedic")
 public class Paramedic extends User {
-    private String specialisation;
+    private String specialization;
     private String academicDegree;
     private String workPlace;
 
