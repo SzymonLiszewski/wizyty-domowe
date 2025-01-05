8950e3b6fdd91372636544b8251241472c910bdf
pawelpp7 <147920372+pawelpp7@users.noreply.github.com>
2025-01-05 15:54:54 +0100

    Update Doctor.java

--- backend/auth/src/main/java/com/medical/homevisits/auth/doctor/entity/Doctor.java
+++ backend/auth/src/main/java/com/medical/homevisits/auth/doctor/entity/Doctor.java
@@ -1,17 +1,21 @@
 package com.medical.homevisits.auth.doctor.entity;
 
+import com.medical.homevisits.auth.patient.entity.Patient;
 import com.medical.homevisits.auth.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 
 @Getter
 @Setter
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
 @DiscriminatorValue("Doctor")
 public class Doctor extends User {
-    private String specialisation;
+    private String specialization;
     private String academicDegree;
     private String workPlace;
 
