54df84f273ca8fd85d6eb4a9e864e67d72a12dba
pawelpp7 <147920372+pawelpp7@users.noreply.github.com>
2025-01-05 15:55:55 +0100

    Update InitializeData.java

--- backend/auth/src/main/java/com/medical/homevisits/auth/user/initialize/InitializeData.java
+++ backend/auth/src/main/java/com/medical/homevisits/auth/user/initialize/InitializeData.java
@@ -1,45 +1,105 @@
 package com.medical.homevisits.auth.user.initialize;
 
+import com.medical.homevisits.auth.patient.entity.Patient;
+import com.medical.homevisits.auth.doctor.entity.Doctor;
+import com.medical.homevisits.auth.nurse.entity.Nurse;
+import com.medical.homevisits.auth.paramedic.entity.Paramedic;
 import com.medical.homevisits.auth.user.entity.User;
 import com.medical.homevisits.auth.user.repository.UserRepository;
-import com.medical.homevisits.auth.user.service.CustomUserDetailsService;
+
+import java.text.DateFormat;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class InitializeData implements InitializingBean {
 
-
-    private final CustomUserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Autowired
-    public InitializeData(
-           CustomUserDetailsService userDetailsService,
-           PasswordEncoder passwordEncoder,
-           UserRepository userRepository
-    ) {
-        this.userDetailsService = userDetailsService;
+    public InitializeData(PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        if (userRepository.findByEmail("test").isEmpty()) {
+        if (userRepository.findByEmail("admin@test.com").isEmpty()) {
             User admin = User.builder()
-                    .email("test")
-                    .password(passwordEncoder.encode("test"))
+                    .email("admin@test.com")
+                    .password(passwordEncoder.encode("admin123"))
                     .build();
             userRepository.save(admin);
             System.out.println("Admin user has been created!");
         }
 
+        if (userRepository.findByEmail("patient@test.com").isEmpty()) {
+            Patient patient = Patient.builder()
+                    .firstName("John")
+                    .lastName("Doe")
+                    .email("patient@test.com")
+                    .password(passwordEncoder.encode("patient123"))
+                    .phoneNumber("012345678")
+                    .dateOfBirth(null)
+                    .build();
+            userRepository.save(patient);
+            System.out.println("Patient user has been created!");
+        }
+
+        if (userRepository.findByEmail("doctor@test.com").isEmpty()) {
+
+            Doctor doctor = Doctor.builder()
+                    .firstName("Jane")
+                    .lastName("Smith")
+                    .email("doctor@test.com")
+                    .password(passwordEncoder.encode("doctor123"))
+                    .phoneNumber("012345678")
+                    .specialization("General Practitioner")
+                    .academicDegree("YES")
+                    .workPlace("clinic")
+                    .dateOfBirth(null)
+                    .build();
+            userRepository.save(doctor);
+            System.out.println("Doctor user has been created!");
+       
+
+        
+        }
+        if (userRepository.findByEmail("nurse@test.com").isEmpty()) {
+            Nurse nurse = Nurse.builder()
+                    .firstName("Alice")
+                    .lastName("Brown")
+                    .email("nurse@test.com")
+                    .phoneNumber("012345678")
+                    .password(passwordEncoder.encode("nurse123"))
+                    .dateOfBirth(null)
+                    .specialization("General Practitioner")
+                    .academicDegree("YES")
+                    .workPlace("clinic")
+                    .doctor("doctor@test.com")
+                    .build();
+            userRepository.save(nurse);
+            System.out.println("Nurse user has been created!");
+        }
+        if (userRepository.findByEmail("paramedic@test.com").isEmpty()) {
+            Paramedic paramedic = Paramedic.builder()
+                    .firstName("Tom")
+                    .lastName("White")
+                    .email("paramedic@test.com")
+                    .email("nurse@test.com")
+                    .password(passwordEncoder.encode("paramedic123"))
+                    .dateOfBirth(null)
+                    .phoneNumber("012345678")
+                    .specialization("General Practitioner")
+                    .academicDegree("YES")
+                    .workPlace("clinic")
+                    .build();
+            userRepository.save(paramedic);
+            System.out.println("Paramedic user has been created!");
+        }
     }
 }
-
