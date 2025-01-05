6ed8dcd0a1c9b5feb2fe5e91515475127f13f94a
pawelpp7 <147920372+pawelpp7@users.noreply.github.com>
2025-01-05 15:55:15 +0100

    Update Nurse.java

--- backend/auth/src/main/java/com/medical/homevisits/auth/nurse/entity/Nurse.java
+++ backend/auth/src/main/java/com/medical/homevisits/auth/nurse/entity/Nurse.java
@@ -1,18 +1,24 @@
-package com.medical.homevisits.auth.paramedic.entity;
+package com.medical.homevisits.auth.nurse.entity;
 
+
+import com.medical.homevisits.auth.doctor.entity.Doctor;
+import com.medical.homevisits.auth.patient.entity.Patient;
 import com.medical.homevisits.auth.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
-@DiscriminatorValue("Paramedic")
-public class Paramedic extends User {
-    private String specialisation;
+@DiscriminatorValue("Nurse")
+public class Nurse extends User {
+    private String specialization;
     private String academicDegree;
+    private String doctor;
     private String workPlace;
 
 }
