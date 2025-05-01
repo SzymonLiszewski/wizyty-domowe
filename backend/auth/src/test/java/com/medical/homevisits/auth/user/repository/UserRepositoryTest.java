5473423ae20de57ec1ca71bfd96ca10d81c08769
Szymon <szymonlis21@gmail.com>
2025-05-01 23:34:51 +0200

    add more tests

--- backend/auth/src/test/java/com/medical/homevisits/auth/user/repository/UserRepositoryTest.java
+++ backend/auth/src/test/java/com/medical/homevisits/auth/user/repository/UserRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.medical.homevisits.auth.user.repository;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.medical.homevisits.auth.doctor.entity.Doctor;
 import com.medical.homevisits.auth.nurse.entity.Nurse;
 import com.medical.homevisits.auth.paramedic.entity.Paramedic;
 import com.medical.homevisits.auth.patient.entity.Patient;
 import com.medical.homevisits.auth.user.entity.User;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,8 +25,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -105,7 +113,8 @@ class UserControllerIntegrationTests{
         mockServer = MockRestServiceServer.createServer(restTemplate);
     }
 
-
+    //transactional (in tests): after every test there is db rollback, so no need to worry about data stored in db
+    @Transactional
     @Test
     public void registerPatientTest() throws Exception {
         String patientJson = """
@@ -131,6 +140,7 @@ class UserControllerIntegrationTests{
                 .andExpect(status().isOk());
     }
 
+    @Transactional
     @Test
     public void loginTest_correctCredentials_200() throws Exception {
         Patient patient = Patient.builder()
@@ -153,6 +163,7 @@ class UserControllerIntegrationTests{
                 .andExpect(status().isOk());
     }
 
+    @Transactional
     @Test
     public void loginTest_wrongPassword_401() throws Exception {
         Patient patient = Patient.builder()
@@ -174,4 +185,136 @@ class UserControllerIntegrationTests{
                         .content(patientJson))
                 .andExpect(status().is4xxClientError());
     }
+
+    @Transactional
+    @Test
+    public void changePassword_correct_200() throws Exception {
+        Patient patient = Patient.builder()
+                .email("patient@email.com")
+                .password(passwordEncoder.encode("patient123"))
+                .build();
+
+        userRepository.save(patient);
+
+        String patientJson = """
+            {
+              "email": "patient@email.com",
+              "password": "patient123",
+              "newPassword": "patient1234"
+            }
+        """;
+
+        // Mocking the appointments service
+        mockServer.expect(requestTo("http://localhost:8082/api/patients"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
+
+        mvc.perform(post("/api/auth/user/password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(patientJson))
+                .andExpect(status().isOk());
+
+        patientJson = """
+            {
+              "email": "patient@email.com",
+              "password": "patient1234"
+            }
+        """;
+
+        mvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(patientJson))
+                .andExpect(status().isOk());
+    }
+
+    @Transactional
+    @Test
+    public void changeUserData_correctToken_200() throws Exception {
+        Patient patient = Patient.builder()
+                .email("patient@email.com")
+                .password(passwordEncoder.encode("patient123"))
+                .build();
+
+        userRepository.save(patient);
+
+        String patientJson = """
+            {
+              "email": "patient@email.com",
+              "password": "patient123"
+            }
+        """;
+
+        MvcResult result =  mvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(patientJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        String token = jsonNode.get("token").asText();
+        System.out.println(token);
+
+        patientJson = """
+            {
+                "email": "changed@gmail.com",
+                "firstName": "changedFirstName",
+                "lastName": "changedLastName",
+                "password": "securePassword",
+                "phoneNumber": "123456789",
+                "dateOfBirth": "2000-01-01",
+                "address": "changed"
+            }
+        """;
+
+        // Mocking the appointments service
+        mockServer.expect(requestTo("http://localhost:8082/api/patients"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
+
+        mvc.perform(post("/api/auth/userData")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(patientJson))
+                        .andExpect(status().isOk());
+
+        User patient1 = userRepository.findByEmail("changed@gmail.com").get();
+        Assertions.assertThat(patient1.getFirstName()).isEqualTo("changedFirstName");
+        Assertions.assertThat(patient1.getLastName()).isEqualTo("changedLastName");
+
+        Optional<User> patientOld = userRepository.findByEmail("patient@email.com");
+        Assertions.assertThat(patientOld).isEmpty();
+    }
+
+    @Transactional
+    @Test
+    public void changePassword_incorrectPassword_401() throws Exception {
+        Patient patient = Patient.builder()
+                .email("patient@email.com")
+                .password(passwordEncoder.encode("patient123"))
+                .build();
+
+        userRepository.save(patient);
+
+        String patientJson = """
+            {
+              "email": "patient@email.com",
+              "password": "incorrect",
+              "newPassword": "patient1234"
+            }
+        """;
+
+        // Mocking the appointments service
+        mockServer.expect(requestTo("http://localhost:8082/api/patients"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
+
+        mvc.perform(post("/api/auth/user/password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(patientJson))
+                .andExpect(status().is4xxClientError());
+    }
+
+
 }
