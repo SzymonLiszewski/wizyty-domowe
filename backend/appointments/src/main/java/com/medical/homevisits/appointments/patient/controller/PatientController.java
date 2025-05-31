1418c50d3cbe1f4e30d0f78fe080869cbb2fcd58
Szymon <szymonlis21@gmail.com>
2025-05-31 13:59:03 +0200

    add function for getting all patients

--- backend/appointments/src/main/java/com/medical/homevisits/appointments/patient/controller/PatientController.java
+++ backend/appointments/src/main/java/com/medical/homevisits/appointments/patient/controller/PatientController.java
@@ -1,26 +1,62 @@
 package com.medical.homevisits.appointments.patient.controller;
 
+import com.medical.homevisits.appointments.doctor.repository.DoctorRepository;
 import com.medical.homevisits.appointments.patient.entity.Patient;
+import com.medical.homevisits.appointments.patient.repository.PatientRepository;
 import com.medical.homevisits.appointments.patient.service.PatientService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.Getter;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
 public class PatientController {
     private final PatientService patientService;
+    private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
 
-    public PatientController(PatientService patientService) {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public PatientController(PatientService patientService, DoctorRepository doctorRepository, PatientRepository patientRepository) {
         this.patientService = patientService;
+        this.doctorRepository = doctorRepository;
+        this.patientRepository = patientRepository;
     }
 
     @PostMapping("/api/patients")
     public void createPatient(@RequestBody createRequest request){
         Patient patient = new Patient(request.getId(), request.getEmail(), request.getPhoneNumber(), request.getFirstName(), request.getLastName());
         patientService.create(patient);
+    }
+
+    @GetMapping("/api/patients")
+    public ResponseEntity<List<Patient>> getPatients(
+            @RequestHeader(value = "Authorization") String token
+    ){
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        String role = claims.get("role", String.class);
+        UUID doctorId = UUID.fromString(claims.get("id", String.class));
+
+        if (!Objects.equals(role, "Doctor") || doctorRepository.findById(doctorId).isEmpty()){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+        List<Patient> patients = patientRepository.findAll();
+        return ResponseEntity.ok(patients);
     }
 
 }
